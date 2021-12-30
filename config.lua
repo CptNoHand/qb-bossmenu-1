a14205dba7a890402a6387dd54ac6dd0c638dc2f
CptNoHand <90973107+CptNoHand@users.noreply.github.com>
2021-12-30 16:28:53 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,12 +1,32 @@
 Config = {}
 
 Config.Jobs = {
-	['police'] = vector3(460.5568, -985.481, 30.728),
-    ['ambulance'] = vector3(347.9575, -1368.63, 42.508)
+	['police'] = vector3(462.05, -985.63, 30.73),
+    ['ambulance'] = vector3(334.86, -593.96, 43.28),
+    ['realestate'] = vector3(-714.75, 261.03, 84.14),
+    ['taxi'] = vector3(903.32, -170.55, 74.0),
+    ['cardealer'] = vector3(-32.0, -1114.2, 26.4),
+    ['mechanic'] = vector3(-339.09, -157.49, 44.59),
+    ['tuner'] = vector3(124.77, -3014.17, 7.04),
+    ['burgershot'] = vector3(-1192.04, -902.476, 13.998),
+    ['bennys'] = vector3(-206.59, -1341.61, 34.89),
+    ['sunrise'] = vector3(-809.96, -208.66, 37.13),
+    ['lsfd'] = vector3(-616.46, -97.5, 45.64),
+    ['vu'] = vector3(93.57, -1292.3, 29.27),
+    ['rooksbar'] = vector3(10.31, 6468.25, 31.55),
+    ['tequilala'] = vector3(-568.577, 291.09, 79.18),
+    ['rockfordrecords'] = vector3(-1007.86, -264.84, 44.8),
+    ['lscustoms'] = vector3(-338.83, -157.5, 44.59),
+    ['redline'] = vector3(-605.41, -919.55, 23.89),
+    ['government'] = vector3(-520.73, -170.02, 42.84),
+    ['catcafe'] = vector3(-578.23, -1067.65, 26.12),
 }
 
 Config.Gangs = {
     ['ballas'] = vector3(118.9943, -1968.81, 21.327),
     ['vagos'] = vector3(358.2892, -2037.97, 25.594),
-	['families'] = vector3(-10.5168, -1429.89, 30.970)
+	['families'] = vector3(-10.5168, -1429.89, 30.970),
+    ['marabunta'] = vector3(1283.49, -1605.55, 25.98),
+    ['rooks'] = vector3(22.93, 6472.1, 31.9),
+    ['empireelite'] = vector3(-775.75, 800.26, 215.16),
 }
